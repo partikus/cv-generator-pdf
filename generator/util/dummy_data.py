ad54fd7524033fd55e4750e2ffd6d062790112dd
wielki.borsuk <wielki.borsuk@gmail.com>
2016-09-17 16:19:25 +0200

    create dummy printout example

---
+++ generator/util/dummy_data.py
@@ -0,0 +1,52 @@
+
+def dummy_employee():
+    return {
+        'firstName': 'Nikoś',
+        'lastName': 'Dyzma',
+        'username': 'ndyzma',
+        'email': 'ndyzma@company.com',
+        'jobTitle': 'multitasking ninja hacker',
+        'details': {
+            'experience': 'none',
+            'education': 'little',
+            'hobbies': 'alcohol'
+        },
+        'skills': [
+            {
+                'skill': {'name': 'PHP', 'description': '', 'url': ''},
+                'startDate': '2012-01-01',
+                'lastUsage': '2013-12-12',
+                'level': 3
+            },
+            {
+                'skill': {'name': 'Java', 'description': '', 'url': ''},
+                'startDate': '2012-01-01',
+                'lastUsage': '2013-12-12',
+                'level': 4
+            },
+            {
+                'skill': {'name': 'CSS', 'description': '', 'url': ''},
+                'startDate': '2012-01-01',
+                'lastUsage': '2013-12-12',
+                'level': 5
+            }
+        ],
+        'languages': [
+            {
+                'name': 'English',
+                'iso': 'en',
+                'level': 4
+            },
+            {
+                'name': 'French',
+                'iso': 'fr',
+                'level': 3
+            },
+            {
+                'name': 'German',
+                'iso': 'de',
+                'level': 1
+            },
+        ],
+        'projects': []
+    }
