f79a38d3f729322761f63e9d91996a73786e6e6d
wielki.borsuk <wielki.borsuk@gmail.com>
2016-09-25 21:16:34 +0200

    create additional dummy entrypoint for pdf files

--- generator/server.py
+++ generator/server.py
@@ -6,6 +6,7 @@ from flask import send_file
 from generator.util.dummy_data import dummy_employee
 from generator import converter
 import os
+import json
 
 
 app = Flask(__name__)
@@ -18,10 +19,25 @@ def index():
     # return "hello"
 
 
-@app.route('/dummy')
-def dummy():
-    model = dummy_employee()
-    return render_template('dummy.jade', model=model)
+def dummy_model(name):
+    files = os.listdir('test')
+    for f in files:
+        if name in f:
+            return json.load(open(os.path.join('test', f)))
+    return dummy_employee()
+
+
+@app.route('/dummy/<name>')
+def dummy(name):
+    model = dummy_model(name)
+    return converter.render_html('dummy.jade', json.dumps(model, ensure_ascii=False))
+
+
+@app.route('/dummy_pdf/<name>')
+def dummy_pdf(name):
+    model = dummy_model(name)
+    output_file = converter.render_pdf('dummy.jade', json.dumps(model, ensure_ascii=False))
+    return send_file(output_file)
 
 
 @app.route('/echo', methods=['POST'])
