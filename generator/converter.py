c099c493ef721681f3310ffb8b249c602b433379
wielki.borsuk <wielki.borsuk@gmail.com>
2016-09-29 15:41:16 +0200

    create a complete report with 'experience' section

--- generator/converter.py
+++ generator/converter.py
@@ -2,6 +2,27 @@ import json
 import tempfile
 from pdfkit import from_string
 import jinja2
+import datetime
+
+
+def format_datetime(value, format='date'):
+    if format == 'date':
+        format = '%Y-%m-%d'
+    elif format == 'month':
+        format = '%Y-%m'
+
+    if isinstance(value, str):
+        value = value.split('+')[0]
+        value = datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S')
+
+    return datetime.datetime.strftime(value, format)
+
+
+def month_range(dummy, since=None, until=None):
+    since = format_datetime(since, 'month') if since else 'Always'
+    until = format_datetime(until, 'month') if until else 'Current'
+
+    return '{} - {}'.format(since, until)
 
 
 def render_wihtout_request(template_name, model):
@@ -9,6 +30,9 @@ def render_wihtout_request(template_name, model):
         loader=jinja2.PackageLoader('generator', 'templates'),
         extensions=['pyjade.ext.jinja.PyJadeExtension']
     )
+    env.filters['months'] = format_datetime
+    env.filters['month_range'] = month_range
+
     template = env.get_template(template_name)
     return template.render(model=model)
 
